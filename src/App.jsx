57ed2af307e7d92654bf3193e4e4746e076bfed2
Kirill Komlev <kirillkomlev02@gmail.com>
2024-06-16 21:12:39 +0300

    connect react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
+import { lazy, Suspense } from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
+
+// const Home = lazy(() => import('./assets/pages/Home/Home'))
+
 export default function App() {
-	return <>123</>
+	return (
+		<>
+			<Routes>
+				<Route
+					exact
+					path='/'
+					element={
+						<Suspense fallback={<Loading />}>
+							<Home />
+						</Suspense>
+					}
+				/>
+
+				<Route
+					exact
+					path='*'
+					element={
+						<Suspense fallback={<Loading />}>
+							<Error />
+						</Suspense>
+					}
+				/>
+			</Routes>
+		</>
+	)
 }
