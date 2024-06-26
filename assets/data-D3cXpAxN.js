4c602674cfd81a97e9f5554bc6a49f441583d19d
Kirill Komlev <kirillkomlev02@gmail.com>
2024-06-26 23:17:34 +0300

    Updates

--- assets/data-D3cXpAxN.js
+++ assets/data-D3cXpAxN.js
@@ -1,4 +1,4 @@
-const A=[{title:"Лучшие фреймворки и библиотеки JavaScript, которыми пользуются все",date:"22 декабря 2023",author:"Журнал «Код",img:"https://dzen.ru/lz5XeGt8f/K9R92bJ52/2f8d1fgEwe4/30GsGQqq6APBcAeAGRKZ53na0yBs2H2whGBbdvWFZojIXRUVmjNiHOeqH9A92EvfgpUh1IOHHjHR8Yh_NEKXKBtWPp_eu2kTDuEWBY1BDCVuzBJwuJ35oEGg2nOzAvJKE4_SFYmk9odl6h3X9hLDGB-PvA1dGIAuRDfd6bGSh_MURzVKRmWrTky4T9KTlh4h8VsT7yGNffeU6Rv1poHjSw1fkxfN4z2D-_1MQzSZRxsexnNAEVTKoY31kWfnHMJ42NQ-zE8rIswFOofZXR4TKLAd0SHqyH18n-ST96BH5EhNVFJbw-53gHg4A4I70U3b0gS0QxOcyinEvNWuI5ZJvt8ascxApG0AjDWOll2QXac32xQsp8Zw8dSrWHLkgWVGzJ5S3APrMwz__0NDOVtFENpF9kiSXc5oRnSep2gbTjgdFvVMQS4pBMD5i9HVlFHkN1SQ52aJ8LeU7ZmwpgpigsfclB8N5fbKM3XABbXRR9meCb8D2dMDpIaxlmRp00J6UJX_j0hkY4VKuIKXVVmVYDIX0yFpTb-8VOvRv63AogFJ3drUz6T8Cbp2QUE1XseSXEKzARSbDy5H9BUg7BBGPdWUOsoMZSEAiLVHk9rfWyY6GxlhJw91OFPhWHYiSWIIDFad2QCkcUY4P8jJvVaJG1RAO8xT1ENhgD_eI-PXQPKYnn4IA-QjzUg4yJMQV14o9xufpCfB_f_eZBi7ZAsljsxZk5GApn1MuX1IjjSQwNJfhn-K0pRHY4j0XGyvl8o239a_AIftYoKHsAYeExUSbLoQlyqiAXp50y-ZPmXJ5ECLVd4YyGO3RPgzgMe9l4LZnAI3AZJRw-gAud1gKV3EP90TfUdI7emNSjwOFlMcnO84ldPsKgA6fVRimnKlT2UAz1dbUMBlO0S7sMiOvVsI1RlC9UFf2QKkwzARoqbcB3LQ3L8DRmQtgoM2xhZS2lFtMpXSb-aJsfHT4tx3b8etQUmWE5HMKr3Jc3NJg7BWSpNbgTqAWhPO7MF5nexm1Qs5Gx7ySAokow0AdQkWVRzTIDIQVyxqBn0-FmJbOiqNakoEHZ3bCmG-CvS7wsm6G4_TEUi3SRLcASaJMZKm5J9CPxGSsgFEraTKAfKBld3WnKh21VytIoh_dhIpVn8kgKvJCd8UVsmp_QG9ssdDsFiB2xqBvI3UWc6oRf1fqeDQjnQQEjZDxyRlCwG2TRUeXtPnuxBb6qxHPfGcrF4_6oLsycOaXFACYX-D_P-AAzGZjRmSyTaCl5nGbIe3HWFo2kk8V509yYkv6keMt8cXEt0eKrpY2eGhRfDwXi0XcioH7olMFVPWiqV3Svv9SgY7l81akUL_SpoRCWSLuh8rK5gCcBNWMgEPJOYGijRFHRZUW2h-0l9l7wJ6PtRgUf3mDyFLzFfdlcXmscz7t4yA_11H2xPIPMueVMwjzvpU4SgbCjyQVfSLQm8uywSywhaQmRTlONBQ7e4Ot3gU5Ri7YIVrx8JQn1EM7PzMMjyHgvwRyt1djbKNExoPp8G3Valpl4O2mFT1x4hq6gQINEdQFV2Q6vcUlu9uDjl-0aCWMm_KL01FmBYQj-d9yjU2yQM6kEhaUkmyT1VTiS8HddekqN-Kd5vUu0xM4ujLTzrL0F5X2mg-2NJtJQhxuRJkE3mnx-4KBZDS1ozmP42x-IkKeZ8L2NvLssLT0A4lBzPZ6-dVj_AWU7uIjiwiSkc2QZZdkBHuMdRYJGnB9HaWJFJ_LEFsg8fZEphDLnMA9PcMB_rYApmcSvIKU9ALpMRwkqsiksP0kBK7DI_lKQ-N9Y9Y1FJbYHdVW-FuifRz0yVatu3KZI_BmVuXxSb9RPO9jAJxkQBZH0A6iByajOdPOpAvaBeJN17W_MuM4qEEgHwN1xLQ3eY_2RJhKEd4Pp2kXjukCqZBTJ8UnAwutYVx-MqIfNzIU5sE84jVG49oyfqWp2PbAj8Wl_qDxKEsRgF2wk",text:`<p>https://dzen.ru/a/ZYU5EDw0kmPN5BDn?sid=160791743114893609</p>
+const l=[{title:"Лучшие фреймворки и библиотеки JavaScript, которыми пользуются все",date:"22 декабря 2023",author:"Журнал «Код",img:"https://dzen.ru/lz5XeGt8f/K9R92bJ52/2f8d1fgEwe4/30GsGQqq6APBcAeAGRKZ53na0yBs2H2whGBbdvWFZojIXRUVmjNiHOeqH9A92EvfgpUh1IOHHjHR8Yh_NEKXKBtWPp_eu2kTDuEWBY1BDCVuzBJwuJ35oEGg2nOzAvJKE4_SFYmk9odl6h3X9hLDGB-PvA1dGIAuRDfd6bGSh_MURzVKRmWrTky4T9KTlh4h8VsT7yGNffeU6Rv1poHjSw1fkxfN4z2D-_1MQzSZRxsexnNAEVTKoY31kWfnHMJ42NQ-zE8rIswFOofZXR4TKLAd0SHqyH18n-ST96BH5EhNVFJbw-53gHg4A4I70U3b0gS0QxOcyinEvNWuI5ZJvt8ascxApG0AjDWOll2QXac32xQsp8Zw8dSrWHLkgWVGzJ5S3APrMwz__0NDOVtFENpF9kiSXc5oRnSep2gbTjgdFvVMQS4pBMD5i9HVlFHkN1SQ52aJ8LeU7ZmwpgpigsfclB8N5fbKM3XABbXRR9meCb8D2dMDpIaxlmRp00J6UJX_j0hkY4VKuIKXVVmVYDIX0yFpTb-8VOvRv63AogFJ3drUz6T8Cbp2QUE1XseSXEKzARSbDy5H9BUg7BBGPdWUOsoMZSEAiLVHk9rfWyY6GxlhJw91OFPhWHYiSWIIDFad2QCkcUY4P8jJvVaJG1RAO8xT1ENhgD_eI-PXQPKYnn4IA-QjzUg4yJMQV14o9xufpCfB_f_eZBi7ZAsljsxZk5GApn1MuX1IjjSQwNJfhn-K0pRHY4j0XGyvl8o239a_AIftYoKHsAYeExUSbLoQlyqiAXp50y-ZPmXJ5ECLVd4YyGO3RPgzgMe9l4LZnAI3AZJRw-gAud1gKV3EP90TfUdI7emNSjwOFlMcnO84ldPsKgA6fVRimnKlT2UAz1dbUMBlO0S7sMiOvVsI1RlC9UFf2QKkwzARoqbcB3LQ3L8DRmQtgoM2xhZS2lFtMpXSb-aJsfHT4tx3b8etQUmWE5HMKr3Jc3NJg7BWSpNbgTqAWhPO7MF5nexm1Qs5Gx7ySAokow0AdQkWVRzTIDIQVyxqBn0-FmJbOiqNakoEHZ3bCmG-CvS7wsm6G4_TEUi3SRLcASaJMZKm5J9CPxGSsgFEraTKAfKBld3WnKh21VytIoh_dhIpVn8kgKvJCd8UVsmp_QG9ssdDsFiB2xqBvI3UWc6oRf1fqeDQjnQQEjZDxyRlCwG2TRUeXtPnuxBb6qxHPfGcrF4_6oLsycOaXFACYX-D_P-AAzGZjRmSyTaCl5nGbIe3HWFo2kk8V509yYkv6keMt8cXEt0eKrpY2eGhRfDwXi0XcioH7olMFVPWiqV3Svv9SgY7l81akUL_SpoRCWSLuh8rK5gCcBNWMgEPJOYGijRFHRZUW2h-0l9l7wJ6PtRgUf3mDyFLzFfdlcXmscz7t4yA_11H2xPIPMueVMwjzvpU4SgbCjyQVfSLQm8uywSywhaQmRTlONBQ7e4Ot3gU5Ri7YIVrx8JQn1EM7PzMMjyHgvwRyt1djbKNExoPp8G3Valpl4O2mFT1x4hq6gQINEdQFV2Q6vcUlu9uDjl-0aCWMm_KL01FmBYQj-d9yjU2yQM6kEhaUkmyT1VTiS8HddekqN-Kd5vUu0xM4ujLTzrL0F5X2mg-2NJtJQhxuRJkE3mnx-4KBZDS1ozmP42x-IkKeZ8L2NvLssLT0A4lBzPZ6-dVj_AWU7uIjiwiSkc2QZZdkBHuMdRYJGnB9HaWJFJ_LEFsg8fZEphDLnMA9PcMB_rYApmcSvIKU9ALpMRwkqsiksP0kBK7DI_lKQ-N9Y9Y1FJbYHdVW-FuifRz0yVatu3KZI_BmVuXxSb9RPO9jAJxkQBZH0A6iByajOdPOpAvaBeJN17W_MuM4qEEgHwN1xLQ3eY_2RJhKEd4Pp2kXjukCqZBTJ8UnAwutYVx-MqIfNzIU5sE84jVG49oyfqWp2PbAj8Wl_qDxKEsRgF2wk",text:`<p>https://dzen.ru/a/ZYU5EDw0kmPN5BDn?sid=160791743114893609</p>
 		<p>Это база</p>
     <p>Сегодня у нас подборка лучших фреймворков и библиотек для JavaScript. Каждый месяц появляются десятки новых, но
       разработчики продолжают работать с проверенными старыми вариантами. Так показывает опрос 2023 года, проведённый
@@ -226,4 +226,104 @@ const A=[{title:"Лучшие фреймворки и библиотеки JavaS
     <p>Вот такой код выведет на экран надпись «Hello, World!»:</p>
     <img
       src="https://dzen.ru/lz5XeGt8f/K9R92bJ52/2f8d1fgEwe4/30GsGQqq6APBcAeAGRKZ53na0yBs2H2whGBbdvWFZojIXRUVmjNiHOeqH9A92EvfgpUh1IOHHjHR8Yh_NEKXKBtWPp_eu2kTDuEWBY1BDHEtmEexOou54ZfhzHNygudehw_SFYmk9odl6h3X9hLDGB-PvA1dGIAuRDfd6bGSh_MURzVKRmWrTky4T9KTlh4h8VsT7yGNffeU6Rv1poHjSw1fkxfN4z2D-_1MQzSZRxsexnNAEVTKoY31kWfnHMJ42NQ-zE8rIswFOofZXR4TKLAd0SHqyH18n-ST96BH5EhNVFJbw-53gHg4A4I70U3b0gS0QxOcyinEvNWuI5ZJvt8ascxApG0AjDWOll2QXac32xQsp8Zw8dSrWHLkgWVGzJ5S3APrMwz__0NDOVtFENpF9kiSXc5oRnSep2gbTjgdFvVMQS4pBMD5i9HVlFHkN1SQ52aJ8LeU7ZmwpgpigsfclB8N5fbKM3XABbXRR9meCb8D2dMDpIaxlmRp00J6UJX_j0hkY4VKuIKXVVmVYDIX0yFpTb-8VOvRv63AogFJ3drUz6T8Cbp2QUE1XseSXEKzARSbDy5H9BUg7BBGPdWUOsoMZSEAiLVHk9rfWyY6GxlhJw91OFPhWHYiSWIIDFad2QCkcUY4P8jJvVaJG1RAO8xT1ENhgD_eI-PXQPKYnn4IA-QjzUg4yJMQV14o9xufpCfB_f_eZBi7ZAsljsxZk5GApn1MuX1IjjSQwNJfhn-K0pRHY4j0XGyvl8o239a_AIftYoKHsAYeExUSbLoQlyqiAXp50y-ZPmXJ5ECLVd4YyGO3RPgzgMe9l4LZnAI3AZJRw-gAud1gKV3EP90TfUdI7emNSjwOFlMcnO84ldPsKgA6fVRimnKlT2UAz1dbUMBlO0S7sMiOvVsI1RlC9UFf2QKkwzARoqbcB3LQ3L8DRmQtgoM2xhZS2lFtMpXSb-aJsfHT4tx3b8etQUmWE5HMKr3Jc3NJg7BWSpNbgTqAWhPO7MF5nexm1Qs5Gx7ySAokow0AdQkWVRzTIDIQVyxqBn0-FmJbOiqNakoEHZ3bCmG-CvS7wsm6G4_TEUi3SRLcASaJMZKm5J9CPxGSsgFEraTKAfKBld3WnKh21VytIoh_dhIpVn8kgKvJCd8UVsmp_QG9ssdDsFiB2xqBvI3UWc6oRf1fqeDQjnQQEjZDxyRlCwG2TRUeXtPnuxBb6qxHPfGcrF4_6oLsycOaXFACYX-D_P-AAzGZjRmSyTaCl5nGbIe3HWFo2kk8V509yYkv6keMt8cXEt0eKrpY2eGhRfDwXi0XcioH7olMFVPWiqV3Svv9SgY7l81akUL_SpoRCWSLuh8rK5gCcBNWMgEPJOYGijRFHRZUW2h-0l9l7wJ6PtRgUf3mDyFLzFfdlcXmscz7t4yA_11H2xPIPMueVMwjzvpU4SgbCjyQVfSLQm8uywSywhaQmRTlONBQ7e4Ot3gU5Ri7YIVrx8JQn1EM7PzMMjyHgvwRyt1djbKNExoPp8G3Valpl4O2mFT1x4hq6gQINEdQFV2Q6vcUlu9uDjl-0aCWMm_KL01FmBYQj-d9yjU2yQM6kEhaUkmyT1VTiS8HddekqN-Kd5vUu0xM4ujLTzrL0F5X2mg-2NJtJQhxuRJkE3mnx-4KBZDS1ozmP42x-IkKeZ8L2NvLssLT0A4lBzPZ6-dVj_AWU7uIjiwiSkc2QZZdkBHuMdRYJGnB9HaWJFJ_LEFsg8fZEphDLnMA9PcMB_rYApmcSvIKU9ALpMRwkqsiksP0kBK7DI_lKQ-N9Y9Y1FJbYHdVW-FuifRz0yVatu3KZI_BmVuXxSb9RPO9jAJxkQBZH0A6iByajOdPOpAvaBeJN17W_MuM4qEEgHwN1xLQ3eY_2RJhKEd4Pp2kXjukCqZBTJ8UnAwutYVx-MqIfNzIU5sE84jVG49oyfqWp2PbAj8Wl_qDxKEsRgF2wk"
-      alt="">`}];export{A as a};
+      alt="">`}],e=[{title:"10 изменений JavaScript за 2023 год, которые вы могли упустить",date:"30 янв 2024",author:"tproger",img:"https://yrokiwp.ru/wp-content/uploads/2023/03/java-modern.jpeg",text:`<p>Рассказали о 10 изменениях в JavaScript и веб-разработке в 2023 году, которые вы могли пропустить. Речь идёт о Next.js, React, Angular, Vue, и Node.js.</p>
+
+<p>Блогер Fireship рассказал о 10 изменениях в JavaScript и веб-разработке в 2023 году, которые вы могли пропустить. Речь в ролике идёт о Next.js, React, Angular, Vue, и Node.js.</p>
+
+<p>Вот, о чём идёт речь в видео:</p>
+
+<ol>
+<li>2023 год оказался бурным: обвал банка, покупка Twitter Илоном Маском, увольнения друзей автора, аресты крипто-энтузиастов, выпуск Cybertruck Tesla и замена ChatGPT на рабочих местах.</li>
+<li>В JavaScript произошли значительные изменения: почти каждый фреймворк решил обновиться в 2023 году.</li>
+<li>Новая функция ECMAScript ‘object group by’ позволяет группировать объекты по определенному признаку, например, по возрасту.</li>
+<li>Добавлены методы ‘array to sorted’, ‘to spliced’ и ‘to reversed’, которые не изменяют исходный массив, в отличие от ‘sort’ и ‘reverse’.</li>
+<li>В HTML появились новые элементы, такие как тег ‘search’ для поисковых форм и элемент ‘dialogue’ для модальных диалогов.</li>
+<li>iOS наконец разрешила веб-приложениям отправлять push-уведомления.</li>
+<li>Next.js обновился до версии 13.4 с новой функцией ‘app directory’, позволяющей получать данные непосредственно в React-компонентах.</li>
+<li>Фреймворк Nuxt.js в экосистеме Vue.js обновил свои инструменты разработчика и предложил компоненты через унифицированную экосистему JavaScript-инструментов.</li>
+<li>Svelte ввел новую функцию ‘Runes’, изменяющую подход к реактивным переменным и управлению состоянием.</li>
+<li>Angular представил новый синтаксис шаблонов, сигналы и ‘deferrable views’ для ленивой загрузки, а также обновил свой логотип.</li>
+<li>Node.js улучшил модель разрешений и добавил собственный WebSocket клиент.</li>
+<li>Bun — новая среда выполнения JavaScript, написанная на Zig, обещает улучшенный опыт разработки и высокую производительность.</li>
+<li>HTMX получил награду ‘JavaScript Framework of the Year’, предлагая подход, сокращающий использование JavaScript.</li>
+</ol>`},{title:"10 изменений JavaScript за 2023 год, которые вы могли упустить",date:"30 янв 2024",author:"tproger",img:"https://yrokiwp.ru/wp-content/uploads/2023/03/java-modern.jpeg",text:`<p>Рассказали о 10 изменениях в JavaScript и веб-разработке в 2023 году, которые вы могли пропустить. Речь идёт о Next.js, React, Angular, Vue, и Node.js.</p>
+
+<p>Блогер Fireship рассказал о 10 изменениях в JavaScript и веб-разработке в 2023 году, которые вы могли пропустить. Речь в ролике идёт о Next.js, React, Angular, Vue, и Node.js.</p>
+
+<p>Вот, о чём идёт речь в видео:</p>
+
+<ol>
+<li>2023 год оказался бурным: обвал банка, покупка Twitter Илоном Маском, увольнения друзей автора, аресты крипто-энтузиастов, выпуск Cybertruck Tesla и замена ChatGPT на рабочих местах.</li>
+<li>В JavaScript произошли значительные изменения: почти каждый фреймворк решил обновиться в 2023 году.</li>
+<li>Новая функция ECMAScript ‘object group by’ позволяет группировать объекты по определенному признаку, например, по возрасту.</li>
+<li>Добавлены методы ‘array to sorted’, ‘to spliced’ и ‘to reversed’, которые не изменяют исходный массив, в отличие от ‘sort’ и ‘reverse’.</li>
+<li>В HTML появились новые элементы, такие как тег ‘search’ для поисковых форм и элемент ‘dialogue’ для модальных диалогов.</li>
+<li>iOS наконец разрешила веб-приложениям отправлять push-уведомления.</li>
+<li>Next.js обновился до версии 13.4 с новой функцией ‘app directory’, позволяющей получать данные непосредственно в React-компонентах.</li>
+<li>Фреймворк Nuxt.js в экосистеме Vue.js обновил свои инструменты разработчика и предложил компоненты через унифицированную экосистему JavaScript-инструментов.</li>
+<li>Svelte ввел новую функцию ‘Runes’, изменяющую подход к реактивным переменным и управлению состоянием.</li>
+<li>Angular представил новый синтаксис шаблонов, сигналы и ‘deferrable views’ для ленивой загрузки, а также обновил свой логотип.</li>
+<li>Node.js улучшил модель разрешений и добавил собственный WebSocket клиент.</li>
+<li>Bun — новая среда выполнения JavaScript, написанная на Zig, обещает улучшенный опыт разработки и высокую производительность.</li>
+<li>HTMX получил награду ‘JavaScript Framework of the Year’, предлагая подход, сокращающий использование JavaScript.</li>
+</ol>`},{title:"10 изменений JavaScript за 2023 год, которые вы могли упустить",date:"30 янв 2024",author:"tproger",img:"https://yrokiwp.ru/wp-content/uploads/2023/03/java-modern.jpeg",text:`<p>Рассказали о 10 изменениях в JavaScript и веб-разработке в 2023 году, которые вы могли пропустить. Речь идёт о Next.js, React, Angular, Vue, и Node.js.</p>
+
+<p>Блогер Fireship рассказал о 10 изменениях в JavaScript и веб-разработке в 2023 году, которые вы могли пропустить. Речь в ролике идёт о Next.js, React, Angular, Vue, и Node.js.</p>
+
+<p>Вот, о чём идёт речь в видео:</p>
+
+<ol>
+<li>2023 год оказался бурным: обвал банка, покупка Twitter Илоном Маском, увольнения друзей автора, аресты крипто-энтузиастов, выпуск Cybertruck Tesla и замена ChatGPT на рабочих местах.</li>
+<li>В JavaScript произошли значительные изменения: почти каждый фреймворк решил обновиться в 2023 году.</li>
+<li>Новая функция ECMAScript ‘object group by’ позволяет группировать объекты по определенному признаку, например, по возрасту.</li>
+<li>Добавлены методы ‘array to sorted’, ‘to spliced’ и ‘to reversed’, которые не изменяют исходный массив, в отличие от ‘sort’ и ‘reverse’.</li>
+<li>В HTML появились новые элементы, такие как тег ‘search’ для поисковых форм и элемент ‘dialogue’ для модальных диалогов.</li>
+<li>iOS наконец разрешила веб-приложениям отправлять push-уведомления.</li>
+<li>Next.js обновился до версии 13.4 с новой функцией ‘app directory’, позволяющей получать данные непосредственно в React-компонентах.</li>
+<li>Фреймворк Nuxt.js в экосистеме Vue.js обновил свои инструменты разработчика и предложил компоненты через унифицированную экосистему JavaScript-инструментов.</li>
+<li>Svelte ввел новую функцию ‘Runes’, изменяющую подход к реактивным переменным и управлению состоянием.</li>
+<li>Angular представил новый синтаксис шаблонов, сигналы и ‘deferrable views’ для ленивой загрузки, а также обновил свой логотип.</li>
+<li>Node.js улучшил модель разрешений и добавил собственный WebSocket клиент.</li>
+<li>Bun — новая среда выполнения JavaScript, написанная на Zig, обещает улучшенный опыт разработки и высокую производительность.</li>
+<li>HTMX получил награду ‘JavaScript Framework of the Year’, предлагая подход, сокращающий использование JavaScript.</li>
+</ol>`},{title:"10 изменений JavaScript за 2023 год, которые вы могли упустить",date:"30 янв 2024",author:"tproger",img:"https://yrokiwp.ru/wp-content/uploads/2023/03/java-modern.jpeg",text:`<p>Рассказали о 10 изменениях в JavaScript и веб-разработке в 2023 году, которые вы могли пропустить. Речь идёт о Next.js, React, Angular, Vue, и Node.js.</p>
+
+<p>Блогер Fireship рассказал о 10 изменениях в JavaScript и веб-разработке в 2023 году, которые вы могли пропустить. Речь в ролике идёт о Next.js, React, Angular, Vue, и Node.js.</p>
+
+<p>Вот, о чём идёт речь в видео:</p>
+
+<ol>
+<li>2023 год оказался бурным: обвал банка, покупка Twitter Илоном Маском, увольнения друзей автора, аресты крипто-энтузиастов, выпуск Cybertruck Tesla и замена ChatGPT на рабочих местах.</li>
+<li>В JavaScript произошли значительные изменения: почти каждый фреймворк решил обновиться в 2023 году.</li>
+<li>Новая функция ECMAScript ‘object group by’ позволяет группировать объекты по определенному признаку, например, по возрасту.</li>
+<li>Добавлены методы ‘array to sorted’, ‘to spliced’ и ‘to reversed’, которые не изменяют исходный массив, в отличие от ‘sort’ и ‘reverse’.</li>
+<li>В HTML появились новые элементы, такие как тег ‘search’ для поисковых форм и элемент ‘dialogue’ для модальных диалогов.</li>
+<li>iOS наконец разрешила веб-приложениям отправлять push-уведомления.</li>
+<li>Next.js обновился до версии 13.4 с новой функцией ‘app directory’, позволяющей получать данные непосредственно в React-компонентах.</li>
+<li>Фреймворк Nuxt.js в экосистеме Vue.js обновил свои инструменты разработчика и предложил компоненты через унифицированную экосистему JavaScript-инструментов.</li>
+<li>Svelte ввел новую функцию ‘Runes’, изменяющую подход к реактивным переменным и управлению состоянием.</li>
+<li>Angular представил новый синтаксис шаблонов, сигналы и ‘deferrable views’ для ленивой загрузки, а также обновил свой логотип.</li>
+<li>Node.js улучшил модель разрешений и добавил собственный WebSocket клиент.</li>
+<li>Bun — новая среда выполнения JavaScript, написанная на Zig, обещает улучшенный опыт разработки и высокую производительность.</li>
+<li>HTMX получил награду ‘JavaScript Framework of the Year’, предлагая подход, сокращающий использование JavaScript.</li>
+</ol>`},{title:"10 изменений JavaScript за 2023 год, которые вы могли упустить",date:"30 янв 2024",author:"tproger",img:"https://yrokiwp.ru/wp-content/uploads/2023/03/java-modern.jpeg",text:`<p>Рассказали о 10 изменениях в JavaScript и веб-разработке в 2023 году, которые вы могли пропустить. Речь идёт о Next.js, React, Angular, Vue, и Node.js.</p>
+
+<p>Блогер Fireship рассказал о 10 изменениях в JavaScript и веб-разработке в 2023 году, которые вы могли пропустить. Речь в ролике идёт о Next.js, React, Angular, Vue, и Node.js.</p>
+
+<p>Вот, о чём идёт речь в видео:</p>
+
+<ol>
+<li>2023 год оказался бурным: обвал банка, покупка Twitter Илоном Маском, увольнения друзей автора, аресты крипто-энтузиастов, выпуск Cybertruck Tesla и замена ChatGPT на рабочих местах.</li>
+<li>В JavaScript произошли значительные изменения: почти каждый фреймворк решил обновиться в 2023 году.</li>
+<li>Новая функция ECMAScript ‘object group by’ позволяет группировать объекты по определенному признаку, например, по возрасту.</li>
+<li>Добавлены методы ‘array to sorted’, ‘to spliced’ и ‘to reversed’, которые не изменяют исходный массив, в отличие от ‘sort’ и ‘reverse’.</li>
+<li>В HTML появились новые элементы, такие как тег ‘search’ для поисковых форм и элемент ‘dialogue’ для модальных диалогов.</li>
+<li>iOS наконец разрешила веб-приложениям отправлять push-уведомления.</li>
+<li>Next.js обновился до версии 13.4 с новой функцией ‘app directory’, позволяющей получать данные непосредственно в React-компонентах.</li>
+<li>Фреймворк Nuxt.js в экосистеме Vue.js обновил свои инструменты разработчика и предложил компоненты через унифицированную экосистему JavaScript-инструментов.</li>
+<li>Svelte ввел новую функцию ‘Runes’, изменяющую подход к реактивным переменным и управлению состоянием.</li>
+<li>Angular представил новый синтаксис шаблонов, сигналы и ‘deferrable views’ для ленивой загрузки, а также обновил свой логотип.</li>
+<li>Node.js улучшил модель разрешений и добавил собственный WebSocket клиент.</li>
+<li>Bun — новая среда выполнения JavaScript, написанная на Zig, обещает улучшенный опыт разработки и высокую производительность.</li>
+<li>HTMX получил награду ‘JavaScript Framework of the Year’, предлагая подход, сокращающий использование JavaScript.</li>
+</ol>`}];export{l as a,e as n};
